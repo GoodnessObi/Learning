53cb50068753bb11f3c5341044c44e6ef56351f3
goodness obi <goodnessobi@gmail.com>
2020-03-23 02:36:25 +0100

    html and css for a calculator

---
+++ javascript frontendmasters/calculator/calculator.js
@@ -0,0 +1,13 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperatot;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick() {}
+
+function init () {
+    document.query
+}
+
+init()
