9c99963d59bb3e752318bbfe66ac23f530c17ff2
goodness obi <goodnessobi@gmail.com>
2020-03-31 22:02:57 +0100

    start js - declare variables - setup functions

--- javascript frontendmasters/calculator/calculator.js
+++ javascript frontendmasters/calculator/calculator.js
@@ -1,13 +1,19 @@
 let runningTotal = 0;
-let buffer = "0";
+let buffer = '0';
 let previousOperatot;
 
 const screen = document.querySelector('.screen');
 
-function buttonClick() {}
+function buttonClick(value) {  }
 
-function init () {
-    document.query
+function handleSymbol() {}
+function handleNumber() {}
+
+function init() {
+    document.querySelector('.calc-buttons')
+    .addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    })
 }
 
 init()
