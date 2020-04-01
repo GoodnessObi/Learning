774128cfc10ded1122979cf72dc806bfdc8de4a3
goodness obi <goodnessobi@gmail.com>
2020-04-01 20:35:38 +0100

    js completed - needs debugging

--- javascript frontendmasters/calculator/calculator.js
+++ javascript frontendmasters/calculator/calculator.js
@@ -1,19 +1,86 @@
 let runningTotal = 0;
 let buffer = '0';
-let previousOperatot;
+let previousOperator;
 
 const screen = document.querySelector('.screen');
 
-function buttonClick(value) {  }
+function buttonClick(value) {
+    if(isNaN(value)) {
+    //this is not a number
+        handleSymbol(value);
+    } else {
+        //this is a number
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
 
-function handleSymbol() {}
-function handleNumber() {}
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = '0';
+            break;
+        case '=':
+            if (previousOperator === null) {
+                //you need two numbers to do maths
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if(buffer === '0') {
+        //do nothing
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    //+buffer
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if(previousOperator === '&plus') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '&minus'){
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '&times') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString) {
+    if(buffer === '0') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
 
 function init() {
     document.querySelector('.calc-buttons')
     .addEventListener('click', function(event) {
         buttonClick(event.target.innerText);
-    })
+    });
 }
 
 init()
