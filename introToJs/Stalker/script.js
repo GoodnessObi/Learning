5a93dc6794a1610173de7c2b653943a5e14c6e5c
GoodnessObi <goodnessobi@gmail.com>
2020-08-16 22:54:02 +0100

    initial setup

--- introToJs/Stalker/script.js
+++
@@ -1,9 +0,0 @@
-var firstName = prompt("Hello there, what is your first name?");
-var lastName = prompt("What is your last name?");
-var age = prompt("How old are you?");
-
-// var fullName = firstName + " " + lastName
-// console.log("Your full name is " + fullName);
-
-console.log("Your full name is " + firstName + " " + lastName);
-console.log("You are "+ age + " years old")
